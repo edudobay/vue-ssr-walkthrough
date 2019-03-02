592ec5384a64462823b732d04043c952b18ebc99
Eduardo Dobay <edudobay@gmail.com>
2019-03-02 20:28:25 -0300

    Make Hello component a bit more interesting

--- Hello.vue
+++ Hello.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>Hello to {{ query.name }}</div>
+  <div>
+    <p>Hello to {{ name }}</p>
+    <h2>Oh dear, that is not your name?</h2>
+    <p>
+      Please correct it here:
+      <input type="text" size="40" v-model="name" />
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Hello',
   props: ['query'],
+  data() {
+    return { name: this.query.name };
+  },
 }
 </script>
